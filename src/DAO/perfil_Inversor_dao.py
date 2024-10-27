d09404764a72472eb28b237370ff0c73d0d61570
Romina Huk <rominahuk@gmail.com>
2024-10-27 17:13:59 -0300

    Restructura del repositorio

--- src/DAO/perfil_Inversor_dao.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from model.perfil_Inversor import PerfilInversor
-
-
-class PerfilInversorDAO(ABC):
-    @abstractmethod
-    def obtener_perfil(self, tipo_inversor):
-        pass    
--- src/model/perfil_inversor.py
+++
@@ -1,7 +0,0 @@
-class PerfilInversor:
-    def __init__(self, id_perfil_inversor, tipo_inversor):
-        self.id_perfil_inversor = id_perfil_inversor
-        self.tipo_inversor = tipo_inversor
-
-    def __str__(self):
-        return f"PerfilInversor({self.id_perfil_inversor}, {self.tipo_inversor})"
